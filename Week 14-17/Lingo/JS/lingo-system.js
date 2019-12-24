2cf72b80216e6eb8451f3d22b42ce62f0d509eb1
Jeremy Korteland <99058701@mydavinci.nl>
2019-12-24 15:17:24 +0100

    Daily Push 16

--- Week 14-17/Lingo/JS/lingo-system.js
+++ Week 14-17/Lingo/JS/lingo-system.js
@@ -5,6 +5,7 @@ var word = words[Math.floor(Math.random()*words.length)];
 var split2 = word.split('');
 var btn = document.getElementById("submit");
 btn.setAttribute("onclick", "submit()");
+
 function startGame(){
 	var container = document.getElementById("wrapper");
 	for(var a=0; a<5; a++){
@@ -12,15 +13,19 @@ function startGame(){
 			var menu = document.createElement("div");
 			menu.setAttribute("id", "box_" + [a] + "_" + [i]);
 			container.appendChild(menu);
-			menu.style.backgroundColor = ("lightgrey");
+			menu.style.backgroundColor = ("#06a4ff");
 			menu.style.color = ("black");
 			menu.style.border = ("1px solid black");
-			menu.style.padding = ("0px 10px 7px 9px");
+			menu.style.padding = ("5px 5px 5px 5px");
 			menu.style.justifyContent = ("center");
-			menu.style.fontSize = ("65px");
+			menu.style.fontSize = ("70px");
+			menu.style.fontFamily = ("Arial");
 			menu.style.fontWeight = ("600");
+			menu.style.textTransform = ("uppercase");
 		}
 	}
+	document.onload = document.getElementById("box_" + [0] + "_" + [0]).innerText = split2[0];
+	document.onload = document.getElementById("box_" + [0] + "_" + [0]).style.backgroundColor = ("rgb(17, 199, 17)");
 }
 function submit() {
 	input = document.getElementById("input").value;
@@ -36,7 +41,7 @@ function submit() {
 				document.getElementById("box_" + [rowCounter] + "_" + [i]).style.borderRadius = ("50%");
 			}
 			else{
-				document.getElementById("box_" + [rowCounter] + "_" + [i]).style.backgroundColor = ("lightgrey");
+				document.getElementById("box_" + [rowCounter] + "_" + [i]).style.backgroundColor = ("#06a4ff");
 			}
 		}
 	rowCounter++;
