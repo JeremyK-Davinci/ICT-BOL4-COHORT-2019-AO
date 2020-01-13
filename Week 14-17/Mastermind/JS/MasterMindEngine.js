b77b725ce6323afdb1f3f40483951d35a8d4b6ac
Jeremy Korteland <99058701@mydavinci.nl>
2020-01-13 10:53:06 +0100

    Daily Push 19

--- Week 14-17/Mastermind/JS/MasterMindEngine.js
+++ Week 14-17/Mastermind/JS/MasterMindEngine.js
@@ -1,51 +1,26 @@
-dragElement(elmnt = document.getElementById("mydiv"));
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    /* if present, the header is where you move the DIV from:*/
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
-    elmnt.onmousedown = dragMouseDown;
-  }
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    /* stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
-function gameStart() {
 var board = document.getElementById("board");
+
+var colors = [
+  "red",
+  "blue",
+  "green",
+  "yellow",
+  "white",
+  "purple"
+]
+for(i=0; i<4; i++){
+var gameCode = colors[Math.floor(Math.random() * colors.length)];
+console.log(gameCode)
+}
+
+
+function gameStart() {
 for(var a = 0; a < 12; a++){
     for(var b = 0; b < 4; b++){
-        var peg = document.createElement("div");
+        var peg = document.createElement("button");
         peg.setAttribute("id", "box_" + a + "_" + b);
-        board.appendChild(peg)
+        peg.addEventListener("click")
+        peg.onclick = peg.style.backgroundColor = "red"
         peg.style.borderRadius = "50px";
         peg.style.backgroundColor = "black";
         peg.style.width = "50px";
@@ -53,7 +28,13 @@ for(var a = 0; a < 12; a++){
         peg.style.display = "inline-block";
         peg.style.marginLeft = "30px";
         peg.style.marginTop = "4px";
+        peg.style.border = "none"
+        board.appendChild(peg)
         }
     }
 }
 gameStart();
+
+function input() {
+
+}
